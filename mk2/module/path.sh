c22f35f04c1e2b5f7f7dd6323e6d9e897bfe2eb3
Brian Koropoff <bkoropoff@gmail.com>
2010-04-22 06:45:36 +0000

    Add sbin dir
    git-svn-id: https://mkbuild.svn.sourceforge.net/svnroot/mkbuild/trunk@57 f95d1f78-9d96-4146-945d-8b56a40d0789

--- mk2/module/path.sh
+++ mk2/module/path.sh
@@ -4,11 +4,13 @@ configure()
     MK_LIB_DIR="`mk_option libdir "${MK_PREFIX_DIR}/lib"`"
     MK_INCLUDE_DIR="`mk_option includedir "${MK_PREFIX_DIR}/include"`"
     MK_BIN_DIR="`mk_option bindir "${MK_PREFIX_DIR}/bin"`"
+    MK_SBIN_DIR="`mk_option sbindir "${MK_PREFIX_DIR}/sbin"`"
 
     mk_msg "prefix: $MK_PREFIX_DIR"
     mk_msg "library dir: $MK_LIB_DIR"
     mk_msg "include dir: $MK_INCLUDE_DIR"
     mk_msg "binary dir: $MK_BIN_DIR"
+    mk_msg "system binary dir: $MK_SBIN_DIR"
 
-    mk_export MK_PREFIX_DIR MK_LIB_DIR MK_INCLUDE_DIR MK_BIN_DIR
+    mk_export MK_PREFIX_DIR MK_LIB_DIR MK_INCLUDE_DIR MK_BIN_DIR MK_SBIN_DIR
 }
