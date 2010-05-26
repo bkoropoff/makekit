b98e68b15704a6c3c74ad2b100969cc0e9ee95d8
Brian Koropoff <bkoropoff@gmail.com>
2010-05-26 06:54:18 +0000

    Load modules when running a script
    git-svn-id: https://mkbuild.svn.sourceforge.net/svnroot/mkbuild/trunk@88 f95d1f78-9d96-4146-945d-8b56a40d0789

--- mk2/script.sh
+++ mk2/script.sh
@@ -2,6 +2,23 @@
 
 . "${MK_HOME}/mk.sh" || exit 1
 
+_mk_load_modules()
+{
+    for _module in `_mk_modules`
+    do
+	unset load
+
+	_mk_source_module "$_module"
+
+	case "`type load 2>&1`" in
+	    *"function"*)
+		MK_MSG_DOMAIN="${_module}"
+		load
+		;;
+	esac
+    done
+}
+
 mk_import
 
 for __dir in ${MK_SEARCH_DIRS}
@@ -11,6 +28,7 @@ do
     then
 	shift
 	mk_parse_params
+	_mk_load_modules
 	. "${__script}"
 	return $?
     fi
