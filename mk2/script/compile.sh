90b2ae8ae52dd96b651c4479b81f33807046a458
Brian Koropoff <bkoropoff@gmail.com>
2010-05-27 08:29:56 +0000

    Prioritize explicit include paths
    git-svn-id: https://mkbuild.svn.sourceforge.net/svnroot/mkbuild/trunk@91 f95d1f78-9d96-4146-945d-8b56a40d0789

--- mk2/script/compile.sh
+++ mk2/script/compile.sh
@@ -4,10 +4,11 @@ _object="$1"
 _source="$2"
 
 EXTRA_CPPFLAGS="-I${MK_STAGE_DIR}${MK_INCLUDEDIR} -DHAVE_CONFIG_H"
+INCLUDE_CPPFLAGS=""
 
 for _dir in ${INCLUDEDIRS}
 do
-    EXTRA_CPPFLAGS="$EXTRA_CPPFLAGS -I${MK_SOURCE_DIR}${MK_SUBDIR}/$_dir -I${MK_OBJECT_DIR}${MK_SUBDIR}/$_dir"
+    INCLUDE_CPPFLAGS="$INCLUDE_CPPFLAGS -I${MK_SOURCE_DIR}${MK_SUBDIR}/$_dir -I${MK_OBJECT_DIR}${MK_SUBDIR}/$_dir"
 done
 
 MK_MSG_DOMAIN="compile"
@@ -26,7 +27,7 @@ fi
 mk_msg "${_source#${MK_SOURCE_DIR}/}"
 mk_mkdir "`dirname "$_object"`"
 _mk_try ${MK_CC} \
-    ${MK_CPPFLAGS} ${CPPFLAGS} ${EXTRA_CPPFLAGS} \
+    ${INCLUDE_CPPFLAGS} ${MK_CPPFLAGS} ${CPPFLAGS} ${EXTRA_CPPFLAGS} \
     ${MK_CFLAGS} ${CFLAGS} ${EXTRA_CFLAGS} \
     ${DEP_FLAGS} \
     -o "$_object" \
