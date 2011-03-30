eecd2f8cf0d836f449b7f42c8341c87af3b4fb11
Brian Koropoff <bkoropoff@gmail.com>
2011-03-30 16:21:39 -0700

    compiler: turn off missing symbol errors on AIX when building a DLO
    Since DLOs can resolve symbols against the program that loads them,
    forcing all symbol references to be resolved at link time is unreasonable.

--- mk/script/link.sh
+++ mk/script/link.sh
@@ -212,7 +212,7 @@ case "${MK_OS}:${LD_STYLE}" in
         COMBINED_LDFLAGS="$COMBINED_LDFLAGS -Wl,-undefined -Wl,dynamic_lookup -Wl,-single_module -Wl,-arch_errors_fatal"
         ;;
     aix:native)
-        DLO_LINK="-shared"
+        DLO_LINK="-shared -Wl,-berok"
         LIB_LINK="-shared"
         COMBINED_LDFLAGS="$COMBINED_LDFLAGS -Wl,-brtl"
 
